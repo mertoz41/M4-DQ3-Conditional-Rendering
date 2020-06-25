af15fd9042e4f77eaaccea45e29d7e12cc7584e5
mertoz41 <mertozkaynak@windowslive.com>
2020-06-25 10:05:27 -0400

    project finished

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const MenuBar = (props) => {
-
   /*
 
   The 'a' tags below are the menu items. Think about the way a menu 
@@ -15,19 +14,20 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+       
+      <a className={props.passBack == "profile" ? "item active" : "item"} id="profile" onClick={props.changeView}>
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a className={props.passBack == "photo" ? "item active" : "item"} id="photo" onClick={props.changeView}>
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
-        <i className="cocktail large icon" id="cocktail"/>
+      <a className={props.passBack == "cocktail" ? "item active" : "item"} id="cocktail" onClick={props.changeView}>
+        <i className="cocktail large icon" id="cocktail" />
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a className={props.passBack == "pokemon" ? "item active" : "item"} id="pokemon" onClick={props.changeView}> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -3,21 +3,47 @@ import MenuBar from '../components/MenuBar.js'
 import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      rendering: "profile"
+    }
+  }
+
+  changeView = (e) => {
+    this.setState({
+      rendering: e.currentTarget.id
+    })
+
+  }
 
 
   render() {
+    let detailsToDisplay
+    if (this.state.rendering === "profile") {
+      detailsToDisplay = < Profile />
+    } else if (this.state.rendering === "photo") {
+      detailsToDisplay = < Photos />
+    } else if (this.state.rendering === "cocktail") {
+      detailsToDisplay = < Cocktails />
+    } else {
+      detailsToDisplay = <Pokemon />
+    }
 
     /*
 
     Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
 
     */
+   
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+
+
+    
 
     return (
       <div>
-        <MenuBar />
+        <MenuBar passBack={this.state.rendering} changeView={this.changeView}/>
         {detailsToDisplay}
       </div>
     )
